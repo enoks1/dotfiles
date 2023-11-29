398d74a2a31528a2eb81e545861c823ed6cbebd9
M. Enes <104064271+en0ks@users.noreply.github.com>
2023-11-29 13:56:22 +0100

    Update init.el

--- init.el
+++ init.el
@@ -88,19 +88,3 @@
 ;; Extra
 (use-package vterm
   :ensure t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
- '(package-selected-packages
-   '(spacemacs-theme monokai-theme zenburn-theme vterm solarized-theme rainbow-mode rainbow-delimiters highlight-numbers evil company centered-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
