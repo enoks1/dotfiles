cf248474da686b40deaea945e3726d1c02974ccc
oeshak <104064271+oeshak@users.noreply.github.com>
2024-02-05 17:18:53 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,21 +45,64 @@
   :config
   (powerline-default-theme))
 
-(use-package moe-theme
-  :disabled t
-  :ensure t
-  :config
-  (add-hook 'text-scale-mode-hook
-            (lambda () (face-remap--remap-face
-                        'line-number)))
-  (add-hook 'text-scale-mode-hook
-            (lambda () (face-remap--remap-face
-                        'line-number-current-line)))
-  (require 'powerline)
-  (setq moe-theme-modeline-color 'black)
-  (powerline-moe-theme)
-  (load-theme 'moe-dark t))
-
 (use-package rainbow-delimiters
   :disabled t
   :ensure t
+  :hook ((prog-mode . rainbow-delimiters-mode)
+         (org-mode . rainbow-delimiters-mode)))
+
+(use-package rainbow-mode
+  :ensure t)
+
+;; Keybinding
+(global-set-key (kbd "C-c c") 'compile)
+(global-set-key (kbd "C-c b") 'eval-buffer)
+(global-set-key (kbd "C-c t") 'transparency)
+(global-set-key (kbd "C-c s") 'shell-command)
+(global-set-key (kbd "C-c i")
+                (lambda ()
+                  (interactive)
+                  (eldoc-minibuffer-message "Indenting buffer...")
+                  (indent-region 0 (point-max))
+                  (eldoc-minibuffer-message "Indenting buffer...done")))
+
+;; Org ;;
+(add-hook 'org-mode-hook
+          (lambda ()
+            (setq
+             display-line-numbers-type 'normal)
+            (display-line-numbers-mode 1)))
+
+;; Programming ;;
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (setq
+             indent-tabs-mode nil
+             c-default-style "bsd"
+             c-basic-offset 4
+             tab-width 4
+             parens-require-spaces nil
+             show-paren-style 'parenthesis
+             whitespace-style '(face tabs spaces tab-mark space-mark trailing indentation empty missing-newline-at-eof))
+            (whitespace-mode 1)
+            (electric-pair-mode 0)
+            (electric-indent-mode 1)
+            (display-line-numbers-mode 1)))
+
+(add-hook 'python-mode-hook (lambda () (setq python-indent-offset 4)))
+
+(use-package company
+  :ensure t
+  :hook ((prog-mode . company-mode)))
+
+(use-package slime
+  :ensure t
+  :config (setq inferior-lisp-program "sbcl"))
+
+;; Custom ;;
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
